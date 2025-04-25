748b76b669fb3c1d5064550e8118df29b5c25d03
ifrucc <ifr.ray@gmail.com>
2025-04-25 14:36:27 +0300

    Update index.js
    update with mongoose and Mongodb Atlas

--- index.js
+++ index.js
@@ -1,69 +1,17 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const path = require('path')
 
+require('dotenv').config()
+const express = require('express')
+
+const path = require('path')
+const Person = require('./models/mongodb')
 
 const app = express()
-
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'build/')));
-morgan.token('content-body', function (req, res) { return JSON.stringify(req.body) })
-const format = ':method :url :status :res[content-length] - :response-time ms :content-body'
-app.use(morgan(format))
 
-let options={
-   
-    methods: 'GET,POST,DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization'] 
-  }
-  
-app.use(cors(options))
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
-let persons = [
 
-    {
-
-        id: 1,
-
-        name: "Arto Hellas",
-
-        number: "040-123456"
-
-    },
-
-    {
-
-        id: 2,
-
-        name: "Ada Lovelace",
-
-        number: "39-44-5323523"
-
-    },
-
-    {
-
-        id: 3,
-
-        name: "Dan Abramov",
-
-        number: "12-43-234345"
-
-    },
-
-    {
-
-        id: 4,
-
-        name: "Mary Poppendieck",
-
-        number: "39-23-6423122"
-
-    }
-
-]
 app.get('/', (req, res) => {
 
     res.sendFile(path.join(__dirname, 'build/', 'index.html'));
@@ -71,73 +19,140 @@ app.get('/', (req, res) => {
 })
 
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
 
-    res.json(persons)
+    Person.find({})
+        .then(result => { res.json(result) })
+        .catch((err) => {
+            console.log("Fetch failed")
+            next(err)
 
-})
-
-
-app.get('/api/persons/:id', (req, res) => {
-
-    const id = Number(req.params.id)
-
-    persons.find(e => e.id === id) ?
-        res.json(persons[id - 1]) :
-        res.status(404).send('Error 404: Contact Not Found')
+        })
 
 })
 
 
-app.get('/info', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
 
-    let response = `Phonebook has info for ${persons.length} persons`
-    response = response + `\n${new Date()}`
-    res.send(response)
+    const id = req.params.id
+
+    Person.findById(id)
+        .then(result => res.json(result))
+        .catch((err) => {
+            console.log("Fetch failed")
+            res.status(400).end()
+            next(err)
+        })
 
 })
 
 
-app.post('/api/persons', (req, res) => {
-
-    const generateId = text => {
-
-        return Number((Array.from(text).map(e => e.charCodeAt(e)).join("")))
-    }
+app.post('/api/persons', (req, res, next) => {
 
     const body = req.body
 
-    if (!body.name || body.name.length < 6 || !body.number || body.number.length < 6) {
-        res.status(400)
-            .send('Error 400: Bad request. "name" or "phone" prop must be at least 6 chars ')
-            .end()
+    Person.exists({ name: body.name })
+        .then(result => {
+            if (result) {
+                return res.status(400).send({ error: 'Name is already exists' })
+            }
+            else {
+                const newcontact = new Person({ name: body.name, number: body.number })
+                newcontact.save()
+                    .then(result => {
+                        res.json(result)
+                    })
+                    .catch((err) => {
+                        res.status(400).send({ error: err.message })
+                        next(err)
+                    })
+            }
+
+        })
+        .catch((err) => {
+            console.log("Fetch failed")
+            next(err)
+
+        })
+})
+
+
+
+
+
+
+app.put('/api/persons/:id', (req, res, next) => {
+
+    const id = req.params.id
+
+    const person = {
+        name: req.body.name,
+        number: req.body.number
+    }
+
+    Person.findByIdAndUpdate(id, person, { new: true, runValidators: true })
+        .then((result) => {
+
+            if (result == null) {
+                res.status(404).end()
+            }
+            else {
+                res.json(result)
+            }
+        })
+        .catch((err) => {
+            res.status(400).send({ error: err.message })
+            next(err)
+        })
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+
+    const id = req.params.id
+
+    Person.findByIdAndDelete(id)
+        .then(result => {
+
+            if (result == null) {
+                res.status(404).end()
+            }
+            else {
+                res.json(result)
+            }
+        })
+        .catch((err) => {
+            res.status(404)
+            next(err)
+        })
+
+})
+
+
+const errorHandler = (error, request, response, next) => {
+
+    if (error.name === 'CastError') {
+
+        console.log('CastError: Id is Not formated correctly')
     }
     else {
 
-        const newcontact = { id: generateId(body.name), name: body.name, number: body.number }
-
-        persons.find(e => e.name === newcontact.name) ?
-            res.status(401).send('Eror 401: Duplicated value. Contatct is existed') :
-            persons = persons.concat(newcontact)
+        console.log(error.message)
     }
-    console.log(persons)
 
-    res.status(200).send('Contact successfully added')
-})
-app.delete('/api/persons/:id',(request, response) => {
-    const id = Number(request.params.id)
-    if (persons.find(e=>e.id===id)){
-        persons= persons.filter(e=> e.id !== id)
-        response.status(200).send('ok')
-    }
-    else{
-        response.status(404).end()
-    }
-    
-   console.log(persons)
+    next()
+}
 
-  })
+
+const unknownEndpoint = (req, res) => {
+
+    res.status(404).send({ error: "Unknow Endpoint/Route not found!" })
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
 
 app.listen(PORT, () => {
+
     console.log(`server running at PORT ${PORT}`)
 })
